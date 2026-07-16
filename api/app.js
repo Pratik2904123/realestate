67b8fce92a80c7cd8bd3ef241574cf1c8878b938
Pratik2904123 <rakhundepratik822@gmail.com>
2026-07-16 14:59:38 +0530

    Trim CLIENT_URL env variable to fix CORS header error

--- api/app.js
+++ api/app.js
@@ -11,7 +11,7 @@ import messageRoute from "./routes/message.route.js";
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL?.trim(), credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
