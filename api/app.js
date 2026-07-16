f6726576d97cf7a7d2b5b407681af4bcea23e8c4
Pratik2904123 <rakhundepratik822@gmail.com>
2026-07-16 15:08:17 +0530

    Temporarily hardcode CORS origin for testing

--- api/app.js
+++ api/app.js
@@ -11,7 +11,7 @@ import messageRoute from "./routes/message.route.js";
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL?.trim(), credentials: true }));
+app.use(cors({ origin: "https://realestate-psi-livid.vercel.app", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
